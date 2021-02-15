8cd741acf9440dcaf2622447a284c5b3e88c63d9
Ediberto <ediberto45gmail.com>
2021-02-15 09:59:36 -0300

    corretion authorization

--- src/midleware/authorization.js
+++ src/midleware/authorization.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 exports.varejao=(req,res,next)=>{
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decode = jwt.verify(token,"key_teste");
+        const decode = jwt.verify(token,process.env.JWT_KEY_varejao);
         next();
     } catch (error) {
         return res.status(401).send({mensagem:"erro na autenticação"});
@@ -13,7 +13,7 @@ exports.varejao=(req,res,next)=>{
 exports.macapa=(req,res,next)=>{
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decode = jwt.verify(token,"key_teste2");
+        const decode = jwt.verify(token,process.env.JWT_KEY_macapa);
         next();
     } catch (error) {
         return res.status(401).send({mensagem:"erro na autenticação"});
